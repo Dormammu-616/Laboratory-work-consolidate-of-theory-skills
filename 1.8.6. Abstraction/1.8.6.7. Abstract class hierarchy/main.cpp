5c5f060b7f4e0aae4daa3d100b7834f7ae0b24fa
Nik <124788493+Dormammu-616@users.noreply.github.com>
2026-03-14 23:41:10 +0300

    Add files via upload

---
+++ 1.8.6. Abstraction/1.8.6.7. Abstract class hierarchy/main.cpp
@@ -0,0 +1,20 @@
+﻿#include "lab 1 - Architectural pattern Taxonomy/Architectural pattern Taxonomy.h"
+#include "lab 2 - Safe state injection/Safe state injection.h"
+#include "lab 3 - Interface separation principle/SmartCityTelemetry.h"
+
+int main()
+{
+	// lab #1
+	Taxonomy tax{};
+	tax.run_lab();
+	
+	// lab #2
+	State_Injection injection{};
+	injection.run_lab();
+
+	// lab #3
+	Smart_City_Telemetry telemetry{};
+	telemetry.run_lab();
+
+	return 0;
+}
