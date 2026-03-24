6dfcb2c55c6e9dcefdb5eb8e00bd4bb2e1843f1e
Nik <124788493+Dormammu-616@users.noreply.github.com>
2026-03-24 17:13:40 +0300

    Add files via upload

---
+++ 1.8.3. Encapsulation/main.cpp
@@ -0,0 +1,20 @@
+﻿#include "lab 1 - access control.h"
+#include "lab 2 - idiom PIMPL.h"
+#include "lab 3 - Stable Public API.h"
+
+int main()
+{
+    // лабораторная #1 - управление доступом
+    Control lab_control{};
+    lab_control.run_lab();
+
+    // лабораторная #2 -
+    Idiom_PIMPL lab_pimpl{};
+    lab_pimpl.run_lab();  
+
+    // лабораторная #3 - 
+    Stable_Public_API api{};
+    api.run_lab();
+
+    return 0;
+}
