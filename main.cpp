3ab65e2b97558cd279f065c353a21f1cd0008f1a
Nik <124788493+Dormammu-616@users.noreply.github.com>
2026-05-16 16:56:00 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,11 @@
+﻿#include "LabRunner.h"
+
+int main()
+{
+    LabRunner::run_lab_forward_list();
+    LabRunner::run_lab_stack();
+    LabRunner::run_lab_queue();
+    LabRunner::run_lab_hash();
+
+    return 0;
+}
