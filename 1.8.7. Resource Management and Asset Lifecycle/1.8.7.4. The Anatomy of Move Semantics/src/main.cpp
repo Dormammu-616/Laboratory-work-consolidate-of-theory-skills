74172503028e668a36c204662dd4ba15b2a3d8bc
Nick <niki.durmo@gmail.com>
2026-09-03 20:40:48 +0300

    chore: setup project structure and cmake

---
+++ 1.8.7. Resource Management and Asset Lifecycle/1.8.7.4. The Anatomy of Move Semantics/src/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "*** Start laboratory #1 -  ***\n";
+	{
+
+	}
+	std::cout << "*** End laboratory #1 ***\n\n";
+
+	std::cout << "*** Start laboratory #2 -  ***\n";
+	{
+
+	}
+	std::cout << "*** End laboratory #2 ***\n\n";
+
+
+	std::cout << "*** Start laboratory #3 -  ***\n";
+	{
+
+	}
+	std::cout << "*** End laboratory #3 ***\n\n";
+
+	return 0;
+}
